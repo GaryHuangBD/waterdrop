8523b406d32998a37dd33f86f3500e889d75e8a8
garyhuang <huangqiang@yy.com>
2020-09-22 11:43:06 +0800

    add example module and check-style plugin

--- waterdrop-apis/src/main/scala/io/github/interestinglab/waterdrop/apis/BaseOutput.scala
+++ waterdrop-apis/src/main/scala/io/github/interestinglab/waterdrop/apis/BaseOutput.scala
@@ -2,7 +2,11 @@ package io.github.interestinglab.waterdrop.apis
 
 import org.apache.spark.sql.{Dataset, Row}
 
-abstract class BaseOutput extends Plugin {
+trait BaseOutput extends Plugin {
+
+  def preProcess(df: Dataset[Row]): Unit = {}
+
+  def afterProcess(df: Dataset[Row]): Unit = {}
 
   def process(df: Dataset[Row])
 }
